01f77cb267e113093d7548dfba69083e3cedbfe5
dcs0spb <s.p.bradley@durham.ac.uk>
2023-10-27 17:52:02 +0100

    Lecture on JavaScript

---
+++ examples/switch.js
@@ -0,0 +1,15 @@
+let x = 11;
+
+switch(x){
+    case 7:
+        alert("seven is a great number");
+        break;
+
+    case 9:
+        alert("Nine is a silly number");
+        break;
+    
+    default:
+        alert(x + " is not any kind of number")
+
+}
