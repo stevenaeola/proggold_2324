78aef9962e91053975e2fe54f8e06ad2f34665c1
dcs0spb <s.p.bradley@durham.ac.uk>
2024-01-25 13:50:17 +0000

    Added random routes from lecture notes

--- examples/server.js
+++ examples/server.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, resp){
-  resp.send('Hello golden cats')
-})
+    resp.send('Hello golden cats')
+  })
+
+app.get('/random/:max', function(req, resp){
+    max = parseInt(req.params.max)
+    rand = Math.floor(Math.random()*max) +1
+    console.log('Max via url is ' + max + ' rand is ' + rand)
+    resp.send('' + rand)
+  })
+
+  app.get('/r', function(req, resp){
+    max = parseInt(req.query.max)
+    rand = Math.floor(Math.random()*max) +1
+    console.log('Max via query is ' + max + ' rand is ' + rand)
+    resp.send('' + rand)
+  })
 
 app.listen(8090)
 console.log('Server running at http://127.0.0.1:8090/');
