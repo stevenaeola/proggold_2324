cc4fb5a9258768391bf1df0e478d7f4780b02fe4
dcs0spb <s.p.bradley@durham.ac.uk>
2024-02-02 17:49:02 +0000

    async version of fetch

--- examples/client/index.js
+++ examples/client/index.js
@@ -2,9 +2,13 @@ alert("loaded index.js")
 
 const button = document.getElementById("all_cats_button")
 
-button.addEventListener('click', function(event){
-  fetch('http://127.0.0.1:8090/cats')
-    .then(response => response.text())
-    .then(body => document.getElementById('all_cats_results').innerHTML=body)
-    .catch( (error) => alert(error))
+button.addEventListener('click', async function(event){
+  try{
+    let response = await fetch('http://127.0.0.1:8090/cats');
+    let body = await response.text();
+    document.getElementById('all_cats_results').innerHTML=body;
+    }
+    catch(error) {
+      alert(error)
+    }
 });
